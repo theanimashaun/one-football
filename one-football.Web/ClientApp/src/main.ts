bbf427a70b160f058d7b792e6faa05624e187efd
Damilola Animashaun <theanimashaun@gmail.com>
2019-03-23 23:43:32 +0100

    Reverse Hard Coding

--- one-football.Web/ClientApp/src/main.ts
+++ one-football.Web/ClientApp/src/main.ts
@@ -5,8 +5,8 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 export function getBaseUrl() {
-  return 'http://localhost:5000/';
-  // return document.getElementsByTagName('base')[0].href;
+  // return 'http://localhost:5000/';
+  return document.getElementsByTagName('base')[0].href;
 }
 
 const providers = [
